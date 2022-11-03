443c20e1594d13458010588dab78498fd789e615
TosiaDan <antonina.ivanets@gmail.com>
2022-11-03 19:14:56 +0100

    del set and get

--- src/js/loadImgService.js
+++ src/js/loadImgService.js
@@ -7,26 +7,25 @@ export default class PixabayApiService {
     this.paginationPage = 1;
   }
 
-  async fetchingAxiosImg() {
-    try {
-      const r = await axios({
-        url: 'https://pixabay.com/api/',
-        params: {
-          key: '30996005-ea40810ea94cfe1a7fe206b35',
-          q: this.searchQueryW,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          per_page: 40,
-          page: this.paginationPage,
-        },
-      });
-      this.addsNotification(r.data.totalHits);
-      this.incrementPage();
-      return r.data.hits;
-    } catch (e) {
-      return console.error(e);
-    }
+  fetchingAxiosImg() {
+    return axios({
+      url: 'https://pixabay.com/api/',
+      params: {
+        key: '30996005-ea40810ea94cfe1a7fe206b35',
+        q: this.searchQueryW,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: this.paginationPage,
+      },
+    })
+      .then(r => {
+        this.addsNotification(r.data.totalHits);
+        this.incrementPage();
+        return r.data.hits;
+      })
+      .catch(e => console.error(e));
   }
 
   incrementPage() {
@@ -48,13 +47,5 @@ export default class PixabayApiService {
     } else if (Math.floor(value / 40) + 1 < this.paginationPage) {
       Notify.info("We're sorry, but you've reached the end of search results.");
     }
-  }
-
-  get searchQueryF() {
-    this.searchQuery;
-  }
-
-  set searchQueryF(newQuery) {
-    this.searchQuery = newQuery;
   }
 }
